47e131cd0378db257ee69f0fe839a39cf5f4fc9d
김광준 <kwangjun3952@gmail.com>
2019-09-18 23:47:54 +0900

    메인 뷰 컨트롤러 생성, 기본 뷰 컨트롤러 삭제

--- TomorrowCloset/TomorrowCloset/AppDelegate.swift
+++ TomorrowCloset/TomorrowCloset/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        
+        let mainVC = MainViewController()
+        
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ TomorrowCloset/TomorrowCloset/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  TomorrowCloset
+//
+//  Created by 김광준 on 18/09/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
